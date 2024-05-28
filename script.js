c9ed212688ff8ae95b37d901a247f2ca1b9daf00
Md Raihan Uddin <95130907+MdRaihanUddin@users.noreply.github.com>
2024-05-28 19:52:08 +0600

    Added a HTML, CSS and JavaScript file.

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+  strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
